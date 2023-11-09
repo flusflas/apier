308ea6351ef4c42645110f332dcb6ce76a1fa92a
Flus Flas <aflusflas@gmail.com>
2023-11-09 13:08:31 +0100

    test: add tests for python api

---
+++ test/templates/python/test_api.py
@@ -0,0 +1,39 @@
+from unittest import mock
+
+from _build.api import API
+from .common import make_json_response
+
+
+def test_create_successfully():
+    """ Creates an API instance successfully. """
+    api = API(host="test-api.com")
+    assert api.host == "https://test-api.com"
+
+
+def test_make_request():
+    """
+    Makes a request to the API.
+    """
+    req_payload = {"foo": "bar"}
+    expected_resp_payload = {
+        "key1": 123,
+        "key2": "hey!"
+    }
+
+    expected_resp = make_json_response(200, expected_resp_payload)
+
+    with mock.patch("requests.request", return_value=expected_resp) as m:
+        resp = (API(host="test-api.com").
+                make_request("POST", "/info", body=req_payload))
+
+    m.assert_called_once_with("POST",
+                              "https://test-api.com/info",
+                              params=None,
+                              headers={
+                                  'Content-Type': 'application/json',
+                              },
+                              data=req_payload,
+                              timeout=3)
+
+    assert resp.status_code == 200
+    assert resp.json() == expected_resp_payload
