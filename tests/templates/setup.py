af281563051a9d8f8e5ee4ddfbecc74ef22c07aa
Flus Flas <aflusflas@gmail.com>
2025-07-31 22:34:31 +0200

    test: implement caching in build_client

--- tests/templates/setup.py
+++ tests/templates/setup.py
@@ -4,16 +4,20 @@ import os
 from apier.core.build import build
 from apier.utils.path import abs_path_from_current_script
 
-built = False
+build_cache = {}
 
 
 def build_client(template: str, definition_file: str):
-    global built
     ctx = {}
     filename = abs_path_from_current_script(f"../definitions/{definition_file}")
 
     # Get the parent directory of the script that calls this function
     caller_path = os.path.dirname(inspect.stack()[1].filename)
     output_path = os.path.join(caller_path, "_build")
+
+    # Use a cache to avoid rebuilding the same template in the same output path
+    if (output_path, template) in build_cache:
+        return
+    build_cache[(output_path, template)] = True
+
     build(ctx, template, filename, output_path)
-    built = True
